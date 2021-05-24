95a2dc647fee8632a69424c29f5ad9fcf1b58ce3
jelissa2007 <jelissa2007@gmail.com>
2021-05-24 05:04:21 -0400

    first draft

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React from 'react';
 import './App.css';
 import './index.css';
 
-function App() {
+import Button1 from "./components/Button1";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jelissa Whatley Bio </h1>
+      <h2>Career Objective</h2>
+      <p>
+        Proven ability to leverage full-stack knowledge and experience to build
+        interactive and user centered websites and application designs to scale.
+        Extensive expertise in large system architecture development and
+        administration, as well as network design and configuration.{" "}
+      </p>
+      <Button1 />
+
+      <button className="btn2">Resume</button>
     </div>
   );
 }
-
-export default App;
